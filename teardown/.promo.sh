106e9396a762a5a8a108e079baf3da9f98f6380e
x4fx77x4f <70858634+x4fx77x4f@users.noreply.github.com>
2023-08-07 17:29:15 +0000

    teardown: allow downloading specific promo file

--- teardown/.promo.sh
+++ teardown/.promo.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-curl 'https://teardowngame.com/promo.php' -H 'Host: promo.teardowngame.com'
+set -e
+if [ -z "$1" ]
+then
+	curl 'https://teardowngame.com/promo.php' -H 'Host: promo.teardowngame.com'
+	exit
+fi
+wget "https://teardowngame.com/$1" --header="Host: promo.teardowngame.com" -O "teardown/.collection/promo.teardowngame.com/$1"
